328d7f407a8c46fa40fb7307271ff58dea26e52b
Lucas Vilas Boas <vilasboaslucas90@gmail.com>
2023-01-15 23:45:45 -0300

    Criando API Rest

---
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
